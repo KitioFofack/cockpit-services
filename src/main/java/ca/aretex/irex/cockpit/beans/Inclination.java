2c49c63102652a35b806a100ef13029c8ab1ad15
Ravelli Tsobgni <cobirav@gmail.com>
2020-11-07 16:34:06 +0100

    Création de la proprieté angle et les getters et setters

--- src/main/java/ca/aretex/irex/cockpit/beans/Inclination.java
+++ src/main/java/ca/aretex/irex/cockpit/beans/Inclination.java
@@ -1,4 +1,17 @@
 package ca.aretex.irex.cockpit.beans;
 
 public class Inclination {
+    private double angle;
+
+    public Inclination(double angle) {
+        this.angle = angle;
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public void setAngle(double angle) {
+        this.angle = angle;
+    }
 }
