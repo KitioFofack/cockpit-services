a3b587166c76755d23a64d28f0196da788371f76
Ravelli Tsobgni <cobirav@gmail.com>
2020-11-12 10:07:28 +0100

    Modification de l'attribut de l'annotation @Api pour la documentation de la classe

--- src/main/java/ca/aretex/irex/cockpit/controllers/LacetServices.java
+++ src/main/java/ca/aretex/irex/cockpit/controllers/LacetServices.java
@@ -4,14 +4,12 @@ import ca.aretex.irex.cockpit.beans.Inclination;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Description;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.DriverManager;
 
-@Api( description ="API les mouvements de Lacet de l'avion")
+@Api( value ="API des mouvements de Lacet de l'avion")
 @Slf4j
 @RestController
 public class LacetServices {
